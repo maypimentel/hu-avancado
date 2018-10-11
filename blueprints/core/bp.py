1fb0ba64945f54e1f688585eb03207130a47bf06
Maycon Pimentel <maycon.pimentel@gmail.com>
2018-10-11 16:21:41 -0300

    Acrescentado codecov ao travis

--- blueprints/core/bp.py
+++ blueprints/core/bp.py
@@ -8,10 +8,5 @@ def hello_world():
     return "Hello world"
 
 
-@core_bp.route('/url')
-def url():
-    return url_for('core.url')
-
-
 def init_app(app: Flask, url_prefix='/'):
     app.register_blueprint(core_bp, url_prefix=url_prefix)
